1f8557da9a64e1c77236e0a7b23a4e1f35b2fe23
FrarilUnited <francefijiadonisbalili@gmail.com>
2021-03-25 06:48:39 -0700

    Third Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,10 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// Primitive
+//	    Primitive
         byte byteVariable = 127;
         short mediumRangeDecimalVariable = 32767;
         int wholeNumbers = 122;
@@ -17,8 +18,62 @@ public class Main {
         int i = 23;
         doubleNumber = i;
 
+        if(isTrueorFalse){
+            System.out.println(veryLargeNumberWithDecimalPlaces);
+        }
+
+//        Switch Statement inside a do while loop
+            Scanner myObj = new Scanner(System.in);
+            char userChoice;
+        do {
+
+            System.out.println("Enter number to determine what day it is:");
+
+            int day = myObj.nextInt();
+
+            switch(day){
+                case 1:
+                    System.out.println("Monday");
+                    break;
+                case 2:
+                    System.out.println("Tuesday");
+                    break;
+                case 3:
+                    System.out.println("Wednesday");
+                    break;
+                case 4:
+                    System.out.println("Thursday");
+                    break;
+                case 5:
+                    System.out.println("Friday");
+                    break;
+                case 6:
+                    System.out.println("Saturday");
+                    break;
+                case 7:
+                    System.out.println("Sunday");
+                    break;
+                default:
+                    System.out.println("You entered number not in choice");
+                    break;
+            }
+
+            System.out.println("Do you want to repeat? Y or N");
+
+
+        }  while(userChoice = )
+
 
         System.out.println(doubleNumber);
         System.out.println(largeDecimalNumbers);
+
+//    Non-Premitive Data Type
+
+//    FizzBuzz Program
+
+
+
+
+
     }
 }
