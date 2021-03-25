f64c0b7554e772c134b17df2445472a3756daaa0
FrarilUnited <francefijiadonisbalili@gmail.com>
2021-03-25 06:09:02 -0700

    Second Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,10 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
 	// Primitive
+        byte byteVariable = 127;
+        short mediumRangeDecimalVariable = 32767;
         int wholeNumbers = 122;
         long largeDecimalNumbers = -12;
+        float numberWithDecimalPlaces = 8.3f;
+        double veryLargeNumberWithDecimalPlaces = 12312312.1243;
+        boolean isTrueorFalse = true;
+        char singleCharacter = '\u0041';
 
-        System.out.println(wholeNumbers);
+        double doubleNumber;
+        int i = 23;
+        doubleNumber = i;
+
+
+        System.out.println(doubleNumber);
         System.out.println(largeDecimalNumbers);
     }
 }
