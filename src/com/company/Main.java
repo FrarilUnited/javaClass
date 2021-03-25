10b966e8c01beaebb2cbdc3a2840b3b644bd2480
FrarilUnited <francefijiadonisbalili@gmail.com>
2021-03-25 05:56:38 -0700

    First Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Primitive
+        int wholeNumbers = 122;
+        long largeDecimalNumbers = -12;
+
+        System.out.println(wholeNumbers);
+        System.out.println(largeDecimalNumbers);
+    }
+}
